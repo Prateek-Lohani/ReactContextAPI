885795cd828d419a77f2227a26d6b293d88f12b6
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-10 00:07:46 +0530

    final commit

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -14,11 +14,11 @@ const {users}=useContext(UserContext);
         <h1 className='font-bold mb-6'>User List</h1>
             {users.map((user)=>{
                 return(
-                    <>
+                    <div key={user.id}>
             <div className='text-md p-2 rounded hover:bg-blue-500 mb-2 w-fit bg-blue-700 text-center'>
                     <Link className='text-white w-full h-full' to={`/user/${user.id}`}>{user.username}</Link>
                     </div>
-                    </>
+                    </div>
                 )
             })}
         
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../utils/Context';
 import { IoIosArrowBack } from "react-icons/io";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UserDetails = () => {
     const {users}=useContext(UserContext);
     const navigate=useNavigate();
-
+    const {id}=useParams();
 
     const backHandler=()=>{
         navigate(-1);    
     }
 
+
   return (<>   
-   <div className='font-bold'>UserDetail</div>
+   <div className='font-bold mb-6'>UserDetail</div>
+   <div className='p-4 w-fit h-fit bg-red-200 rounded-lg'>
+    <p> <span className='font-serif text-green-700'>UserName:</span> {users[id].username}</p>
+    <p><span className='font-serif text-green-700'>City :</span> {users[id].city}</p>
+   </div>
     <button onClick={backHandler} className='mt-6 w-fit px-4 py-1 text-center bg-blue-700'><IoIosArrowBack/></button>
   </>
 
